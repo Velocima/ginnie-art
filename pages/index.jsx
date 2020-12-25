2bd5d1850d3b8615dbec04088f015166fb7e9176
Velo <hartley.max@outlook.com>
2020-12-25 23:04:22 +0000

    some boilerplate removed

---
+++ pages/index.jsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+export default function Home() {
+	return (
+		<div>
+			<Head>
+				<title>Virginnie Hartley</title>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+			<main>
+				<h1>Virginnie Hartley</h1>
+				<h2>Artist</h2>
+				<h2>Illustrators</h2>
+			</main>
+		</div>
+	);
+}
