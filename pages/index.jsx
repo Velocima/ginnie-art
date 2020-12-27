8587ca80d5d618f7fd349575b45271e28faa88f5
Velo <hartley.max@outlook.com>
2020-12-27 17:55:27 +0000

    basic styling added to landing page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Nav from '../components/nav';
+import style from '../styles/index.module.css';
 
 export default function Home() {
 	return (
@@ -10,9 +11,23 @@ export default function Home() {
 			</Head>
 			<Nav />
 			<main>
-				<h1>Virginnie Hartley</h1>
-				<h2>Artist</h2>
-				<h2>Illustrators</h2>
+				<section class={style.banner}>
+					<h1>Virginnie Hartley</h1>
+					<h2>Artist</h2>
+					<h2>Illustrator</h2>
+				</section>
+				<section class={style.about}>
+					<img src='images/profile.jpg' alt='Artist and illustrator Virginnie Hartley' />
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, beatae
+						odio mollitia fuga inventore dolore error eos sapiente blanditiis architecto
+						ipsum voluptates expedita. Inventore repudiandae impedit perspiciatis cum,
+						expedita quas. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Laborum autem soluta reprehenderit, numquam corporis nam esse dolore
+						veritatis cum earum dolorum, voluptates libero itaque eaque! Enim quibusdam
+						sapiente dolores deleniti?
+					</p>
+				</section>
 			</main>
 		</div>
 	);
