81cb79aebbdaaf708cf6d055251cbec3c50c133a
Velo <hartley.max@outlook.com>
2020-12-28 01:13:53 +0000

    name spelling corrected on index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,7 +6,7 @@ export default function Home() {
 	return (
 		<div>
 			<Head>
-				<title>Virginnie Hartley</title>
+				<title>Virginie Hartley</title>
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 			<Nav />
