52c1035820c5beeb32a3f73f4b9aa54a30be77cf
Velo <hartley.max@outlook.com>
2020-12-28 13:21:00 +0000

    react icons added for email icon. contact page content added and basic styling applied

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,5 +1,8 @@
 import Nav from '../components/nav';
 import Head from 'next/head';
+import style from '../styles/contact.module.css';
+import { GoMail } from 'react-icons/go';
+import { IconContext } from 'react-icons';
 
 export default function Contact() {
 	return (
@@ -8,7 +11,18 @@ export default function Contact() {
 				<title>Virginie Hartley | Contact</title>
 			</Head>
 			<Nav></Nav>
-			<main>Contact</main>
+			<main>
+				<section className={style.banner}>
+					<h1>Contact Me</h1>
+				</section>
+				<section className={style.contactInfo}>
+					<a href=''>
+						<IconContext.Provider value={{ className: style.icon }}>
+							<GoMail /> email@email.com
+						</IconContext.Provider>
+					</a>
+				</section>
+			</main>
 		</>
 	);
 }
