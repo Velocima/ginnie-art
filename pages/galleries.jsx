d3c55177e67f01828def82c55256a3619771a09d
Velo <hartley.max@outlook.com>
2020-12-28 01:10:21 +0000

    links to dynamically routed galleries implimented on galleries page

--- pages/galleries.jsx
+++ pages/galleries.jsx
@@ -1,36 +1,37 @@
 import Link from 'next/link';
 import Nav from '../components/nav';
 import style from '../styles/galleries.module.css';
+import galleries from '../galleries/galleries';
 
-export default function Contact() {
+export function getStaticProps() {
+	return {
+		props: {
+			galleriesData: galleries,
+		},
+	};
+}
+
+export default function Contact({ galleriesData }) {
 	return (
 		<>
 			<Nav></Nav>
 			<main className={style.main}>
-				<Link href='/gallery/pageone'>
-					<a className={style.link}>
-						<img className={style.img} src='images/gallery/wallpaper1.jpg' alt='' />
-						<div className={style.overlay}>
-							<p>Page One</p>
-						</div>
-					</a>
-				</Link>
-				<Link href='/gallery/pageone'>
-					<a className={style.link}>
-						<img className={style.img} src='images/gallery/wallpaper1.jpg' alt='' />
-						<div className={style.overlay}>
-							<p>Page One</p>
-						</div>
-					</a>
-				</Link>
-				<Link href='/gallery/pageone'>
-					<a className={style.link}>
-						<img className={style.img} src='images/gallery/wallpaper1.jpg' alt='' />
-						<div className={style.overlay}>
-							<p>Page One</p>
-						</div>
-					</a>
-				</Link>
+				{galleriesData.map((gallery) => {
+					return (
+						<Link href={`/gallery/${gallery.title}`}>
+							<a className={style.link}>
+								<img
+									className={style.img}
+									src={gallery.images[0].image}
+									alt={gallery.images[0].alt}
+								/>
+								<div className={style.overlay}>
+									<p>{gallery.title}</p>
+								</div>
+							</a>
+						</Link>
+					);
+				})}
 			</main>
 		</>
 	);
