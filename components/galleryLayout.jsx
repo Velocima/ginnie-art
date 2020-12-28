646dc5e6fa2258cf4c7516260c27fd1b96a1211b
Velo <hartley.max@outlook.com>
2020-12-28 01:01:51 +0000

    modal implemented for gallery pages

--- components/galleryLayout.jsx
+++ components/galleryLayout.jsx
@@ -1,7 +1,31 @@
+import { useState, useEffect } from 'react';
 import Nav from './nav';
 import style from '../styles/gallery.module.css';
 
 export default function GalleryLayout({ images, title }) {
+	const [isModalOpen, setIsModalOpen] = useState(false);
+	const [modalImage, setModalImage] = useState(images[0]);
+
+	const handleImageClick = ({ target }) => {
+		setIsModalOpen(true);
+		setModalImage({ image: target.src, alt: target.alt });
+	};
+	const closeModal = () => {
+		setIsModalOpen(false);
+	};
+
+	useEffect(() => {
+		const handleEscape = ({ key }) => {
+			if (key === 'Escape') {
+				setIsModalOpen(false);
+			}
+		};
+		window.addEventListener('keydown', handleEscape);
+		return () => {
+			window.removeEventListener('keydown', handleEscape);
+		};
+	}, [isModalOpen]);
+
 	return (
 		<>
 			<Nav />
@@ -13,11 +37,26 @@ export default function GalleryLayout({ images, title }) {
 					{images.map((image) => {
 						return (
 							<div className={style.overflowContainer}>
-								<img src={image.image} alt={image.alt} className={style.image} />
+								<img
+									src={image.image}
+									alt={image.alt}
+									className={style.image}
+									onClick={handleImageClick}
+								/>
 							</div>
 						);
 					})}
 				</section>
+				<div
+					className={style.modal}
+					style={{
+						opacity: isModalOpen ? '1' : '0',
+						pointerEvents: isModalOpen ? 'all' : 'none',
+					}}
+				>
+					<button onClick={closeModal}>X</button>
+					<img src={modalImage.image} alt={modalImage.alt} />
+				</div>
 			</main>
 		</>
 	);
